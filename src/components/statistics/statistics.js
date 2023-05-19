27272b08237283875e1772208f5cd87dfafa5cdd
Kravets_Dmytro <ditur@ukr.net>
2023-05-19 12:42:59 +0300

    +css ststs

--- src/components/statistics/statistics.js
+++ src/components/statistics/statistics.js
@@ -1,19 +1,30 @@
 import PropTypes from 'prop-types';
 import css from './statistics.module.css';
 
+function generateRandomColor() {
+  let color = '#';
+  for (let i = 0; i < 3; i++)
+    color += (
+      '0' + Math.floor((Math.random() * Math.pow(16, 2)) / 2).toString(16)
+    ).slice(-2);
+  return color;
+}
+
+
  function Statistics({title, stats}){
 return (
-<section className="statistics">
-{title &&  <h2 className="title">{title}</h2> }
-<ul className="css">
+<section className={css.statistics}>
+{title &&  <h2 className={css.title}>{title}</h2> }
+<ul className={css['stat-list']}>
 {stats.map(stat => (
           <li
             key={stat.id}
-            className="css"
-
+            className={css.item}
+            style={{backgroundColor: generateRandomColor(),
+            }}
           >
-            <span className="css">{stat.label}</span>
-            <span className="css">{stat.percentage}%</span>
+            <span className={css.label}>{stat.label}</span>
+            <span className={css.percentage}>{stat.percentage}%</span>
           </li>
         ))}
       </ul>
