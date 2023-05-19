7d68d6973c2c62830c72e406e7aaecf3ffc00aaa
Kravets_Dmytro <ditur@ukr.net>
2023-05-19 12:32:54 +0300

    comit 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,8 @@ import Statistics from './statistics/statistics.js';
 import data from './statistics/data.json';
 import FriendList from "./friendList/FriendList.js";
 import friends from './friendList/friends.json';
+import transactions from './TransactionHistory/transactions.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory.jsx';
 
 export const App = () => {
 
@@ -20,6 +22,7 @@ stats={user.stats}/>
 
 <Statistics title="UPLOAD STATS" stats={data} />
 <FriendList friends={friends} />
+<TransactionHistory items={transactions} />;
 
 
    </div>
